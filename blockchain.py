742785ce978a66ce41a1a34bc3ea97710f084191
jaskirannagi <32842072+jaskirannagi@users.noreply.github.com>
2017-11-06 19:47:11 +0000

    Add files via upload

--- blockchain.py
+++ blockchain.py
@@ -1,36 +1,59 @@
 import hashlib, json, sys
 import copy
-setup=0;
-
-
-def geninissbloc():
-    state = {u'Alice':50, u'Bob':50}  # Define the initial state
-    genesisBlockTxns = [state]
-    genesisBlockContents = {u'blockNumber':0,u'parentHash':None,u'txnCount':1,u'txns':genesisBlockTxns}
-    genesisHash = hashMe( genesisBlockContents )
-    genesisBlock = {u'hash':genesisHash,u'contents':genesisBlockContents}
-    genesisBlockStr = json.dumps(genesisBlock, sort_keys=True)
-    chain = [genesisBlock]
-    
-    
-    
-def hashMe(msg=""):
-    # For convenience, this is a helper function that wraps our hashing algorithm
-    if type(msg)!=str:
-        msg = json.dumps(msg,sort_keys=True)  # If we don't sort keys, we can't guarantee repeatability!
-        
-    if sys.version_info.major == 2:
-        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
-    else:
-        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
-
-def makeblock
-
-
 import random
-random.seed(0)
+import RPi.GPIO as GPIO
+import time
 
-def makeTransaction(maxValue=3):
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT) #blue
+GPIO.setup(27,GPIO.OUT) #green
+GPIO.setup(22,GPIO.OUT)#red
+GPIO.setup(4,GPIO.IN) #switch
+setup=0;
+buffcount=0;
+transaction=false;
+start_time = 0;
+
+def geninissbloc():  #create the first block for the chain
+    state = {u'raspA':50, u'raspB':50,u'raspC':50}  # Define the initial state
+    genesisBlockTxns = [state]  #initial block transactions is equal to the sum of all initial transactions
+    genesisBlockContents = {u'blockNumber':0,u'parentHash':None,u'txnCount':1,u'txns':genesisBlockTxns} #dictionary for all the block details
+    genesisHash = hashMe( genesisBlockContents ) #hash of the block for the first chain
+    genesisBlock = {u'hash':genesisHash,u'contents':genesisBlockContents} #hash of first block with block hash (hash to test changes, block to continue chain)
+    genesisBlockStr = json.dumps(genesisBlock, sort_keys=True) #orgaise the dictionary in chronological order so that the hash is the same each time it is done.
+    chain = [genesisBlock] #chain is the genisis block and hash. (can change and check the chain with the two array entries)
+    
+    
+    
+def hashMe(msg=""): #hashing function for the 256 SHA- algorithm
+    # For convenience, this is a helper function that wraps our hashing algorithm
+    if type(msg)!=str: #if the msg is not a string (public or private keys)
+        msg = json.dumps(msg,sort_keys=True)  # If we don't sort keys, we can't guarantee repeatability! 
+        #other nodes may not have the block info in chronolgical order, order then execute. This means it is accurate to repeat.
+        
+    if sys.version_info.major == 2: #python version
+        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8') #sha256 hash on the block msg
+    else:
+        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()#sha256 hash on the block msg
+
+
+    
+def maketestBlock(txns,chain): #make the first block to add to the chain
+    parentBlock = chain[-1]#parentblock is last element in the chain 
+    parentHash  = parentBlock[u'hash'] #parent hash is last hash in last chain element
+    blockNumber = parentBlock[u'contents'][u'blockNumber'] + 1 #the block number is block number in the contents of the latest chain element +1.
+    txnCount    = len(txns)#count the amount of transactions happend so far
+    blockContents = {u'blockNumber':blockNumber,u'parentHash':parentHash,
+                     u'txnCount':len(txns),'txns':txns} #update the block contents
+    blockHash = hashMe( blockContents ) #hash the block contents
+    block = {u'hash':blockHash,u'contents':blockContents} # block is equal to a 2d dictionary of the hash and hash contents.
+    
+    return block    
+
+
+
+def makeTransaction(maxValue=3):#randomly generate a transaction to test the system
     # This will create valid transactions in the range of (1,maxValue)
     sign      = int(random.getrandbits(1))*2 - 1   # This will randomly choose -1 or 1
     amount    = random.randint(1,maxValue)
@@ -38,38 +61,45 @@ def makeTransaction(maxValue=3):
     bobPays   = -1 * alicePays
     # By construction, this will always return transactions that respect the conservation of tokens.
     # However, note that we have not done anything to check whether these overdraft an account
-    txnBuffer = [makeTransaction() for i in range(30)] #add to generic array to create a buffer
+   
     return {u'Alice':alicePays,u'Bob':bobPays}
     
+    
+    
+    
+def flooddetect(nodee='',flooded,node1='',node2=''):#randomly generate a transaction to test the system
+
+    return {nodee:flooded,node1:0,node2:0}    
+    
 
 
-def transactionbuffer() #call every loop, will reset tranasaction buffer when enough transactions can generate a block
+def transactionbuffer(): #call every loop, will reset tranasaction buffer when enough transactions can generate a block
     blockSizeLimit = 5  # Arbitrary number of transactions per block- 
                    #  this is chosen by the block miner, and can vary between blocks!
-    while len(txnBuffer) > 0:
-        bufferStartSize = len(txnBuffer)
+    while len(txnBuffer) > 0: # for the length of the buffer
+        bufferStartSize = len(txnBuffer) #get size of array
     
         ## Gather a set of valid transactions for inclusion
-        txnList = []
-        while (len(txnBuffer) > 0) & (len(txnList) < blockSizeLimit):
-            newTxn = txnBuffer.pop()
+        txnList = [] # empty the list
+        while (len(txnBuffer) > 0) & (len(txnList) < blockSizeLimit): # while there is transactions and the transaction list (array to be added to the block) is less than 5
+            newTxn = txnBuffer.pop() #take trasnaction off the array and set it equal to new transaction variable
             validTxn = isValidTxn(newTxn,state) # This will return False if txn is invalid
         
             if validTxn:           # If we got a valid state, not 'False'
-                txnList.append(newTxn)
-                state = updateState(newTxn,state)
+                txnList.append(newTxn) #add transaction to new transactiion list
+                state = updateState(newTxn,state) #update the state of the the ledger
             else:
                 print("ignored transaction")
                 sys.stdout.flush()
                 continue  # This was an invalid transaction; ignore it and move on
         
         ## Make a block
-        myBlock = makeBlock(txnList,chain)
-        chain.append(myBlock)
+        myBlock = makeBlock(txnList,chain) #create the bock from the details
+        chain.append(myBlock) #add the block to the chain
         
 
 
-def updateState(txn, state):
+def updateState(txn, state): # state (number of transactions, users, etc)
     # Inputs: txn, state: dictionaries keyed with account names, holding numeric values for transfer amount (txn) or account balance (state)
     # Returns: Updated state, with additional users added to state if necessary
     # NOTE: This does not not validate the transaction- just updates the state!
@@ -77,15 +107,15 @@ def updateState(txn, state):
     # If the transaction is valid, then update the state
     state = state.copy() # As dictionaries are mutable, let's avoid any confusion by creating a working copy of the data.
     for key in txn:
-        if key in state.keys():
-            state[key] += txn[key]
+        if key in state.keys(): #.keys refers to json, i.e. cell positions in an array
+            state[key] += txn[key]#if state  corect, make it = chain + next transactions
         else:
-            state[key] = txn[key]
+            state[key] = txn[key] #if state not corect, make it = to the chain state
     return state
 
 
 
-def isValidTxn(txn,state):
+def isValidTxn(txn,state): #check the transaction (or switch signal) is OK.
     # Assume that the transaction is a dictionary keyed by account names
 
     # Check that the sum of the deposits and withdrawals is 0
@@ -104,17 +134,7 @@ def isValidTxn(txn,state):
     return True
 
 
-def makeBlock(txns,chain):
-    parentBlock = chain[-1]
-    parentHash  = parentBlock[u'hash']
-    blockNumber = parentBlock[u'contents'][u'blockNumber'] + 1
-    txnCount    = len(txns)
-    blockContents = {u'blockNumber':blockNumber,u'parentHash':parentHash,
-                     u'txnCount':len(txns),'txns':txns}
-    blockHash = hashMe( blockContents )
-    block = {u'hash':blockHash,u'contents':blockContents}
-    
-    return block
+
 
 
 
@@ -145,12 +165,12 @@ def checkBlockValidity(block,parent,state):
         else:
             raise Exception('Invalid transaction in block %s: %s'%(blockNumber,txn))
 
-    checkBlockHash(block) # Check hash integrity; raises error if inaccurate
+    checkBlockHash(block) #Check hash integrity; raises error if inaccurate
 
-    if blockNumber!=(parentNumber+1):
+    if blockNumber!=(parentNumber+1): #check correct block number
         raise Exception('Hash does not match contents of block %s'%blockNumber)
 
-    if block['contents']['parentHash'] != parentHash:
+    if block['contents']['parentHash'] != parentHash: #check corrent hash
         raise Exception('Parent hash not accurate at block %s'%blockNumber)
     
     return state
@@ -203,20 +223,41 @@ def checkChain(chain):
 
 
 
-while(1)
+while(1):
     
-    if(setup==0)
-        geninissbloc()
-        setup=1
+    if(setup==0):  #run setup (only once)
+        geninissbloc() #set up starting bloc
+        setup=1 #dont run again
+    nodeBchain = copy.copy(chain) 
+    
+    if (GPIO.input(4)!=swstate) #if the switch has changed state 
+        if (GPIO.input(4)): #if flood detection is on
+            txnBuffer[buffcount] = makeTransaction('raspA',50,'raspB','raspC') #send the id number 50 (flooded) to the chain.
+            buffcount=buffcount+1 #increment once to update the transaction buffer
         
+        else (!GPIO.input(4)) #if flood detection is off
+            txnBuffer[buffcount] = makeTransaction('raspA',100,'raspB','raspC') #send the id number 100 (not flooded) to the chain.
+            buffcount=buffcount+1 #increment once to update the transaction buffer
+        swstate=GPIO.input(4)# the transaction is only recorded if the switch state has been changed.
+        
+    if (time.time()-start_time>10)#every 10 seconds
+        transactionbuffer() #check if number of transactions/block is complete and then if so, generate a new block and add to the chain.
+        #every 10 seconds the state changes are recorded, the transaction will be read and added to the block chain. if more than 5 transaction
+        #in the time period are set off, only 5 transactions / block will be added to the chain (queue the transactions.)
+        buffcount=0
+        start_time = time.time()#present time
     
-    nodeBchain = copy.copy(chain)
-    nodeBtxns  = [makeTransaction() for i in range(5)]
-    newBlock   = makeBlock(nodeBtxns,nodeBchain)
-    transactionbuffer()
+    ############ test code to test block generation (randomizes payment from two people) 
+    nodeBtxns  = [makeTransaction() for i in range(5)] #create an array to store the transactions that happen. the buffer will check and create the block when ready and empty
+    newBlock   = maketestBlock(nodeBtxns,nodeBchain)#adds the transactions to the current block
+    ########## it will add the transaction to the block.
+    
+   
+   
+    ### broadcast chain to all nodes
+    ### recieve chain from all nodes
 
-
-
+    newBlock= #recieve block from other nodes
 
 
     print("Blockchain on Node A is currently %s blocks long"%len(chain))
